f4f59458654640fe4cb1feec6bbea4b52edb1bc5
Artur HRYTSENKO <ahrytsen@e2r13p10.unit.ua>
2017-11-08 22:14:42 +0200

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/30 19:17:46 by ahrytsen          #+#    #+#             */
-/*   Updated: 2017/11/02 22:24:31 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2017/11/08 22:10:43 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,25 @@
 
 int	ft_atoi(const char *str)
 {
-	int				sign;
-	unsigned long	res;
+	int		i;
+	int		sign;
+	long	res;
 
 	sign = 0;
 	res = 0;
+	i = 0;
 	while (*str == ' ' || (*str > 8 && *str < 14))
 		str++;
-	while (*str)
+	if (!sign && (*str == '-' || *str == '+'))
+		sign = (*str++ == '-' ? -1 : 1);
+	while (*str >= '0' && *str <= '9')
 	{
-		if (!sign && (*str == '-' || *str == '+'))
-			sign = (*str == '-' ? -1 : 1);
-		else if (*str >= '0' && *str <= '9')
-		{
-			res = res * 10 + (*str - '0');
-			sign = (sign == -1 ? -1 : 1);
-		}
-		else
-			break ;
+		res = res * 10 + (*str - '0');
+		sign = (sign == -1 ? -1 : 1);
+		i += (res ? 1 : 0);
 		str++;
+		if (i > 19 || res < 0 || (i && !res))
+			return (sign == 1 ? -1 : 0);
 	}
-	if (res > MAX_LONG)
-		return (sign == 1 ? -1 : 0);
 	return (sign * (int)res);
 }
