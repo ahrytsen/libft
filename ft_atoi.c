3f1095c24f74a2f6cb673b4d2a5bfafdf4e39480
Artur HRYTSENKO <ahrytsen@e2r13p10.unit.ua>
2017-11-11 23:14:15 +0200

    atoi in 20 lines =)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/30 19:17:46 by ahrytsen          #+#    #+#             */
-/*   Updated: 2017/11/08 22:10:43 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2017/11/11 23:04:16 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,17 @@ int	ft_atoi(const char *str)
 	int		sign;
 	long	res;
 
-	sign = 0;
+	sign = 1;
 	res = 0;
 	i = 0;
 	while (*str == ' ' || (*str > 8 && *str < 14))
 		str++;
-	if (!sign && (*str == '-' || *str == '+'))
+	if (*str == '-' || *str == '+')
 		sign = (*str++ == '-' ? -1 : 1);
 	while (*str >= '0' && *str <= '9')
 	{
-		res = res * 10 + (*str - '0');
-		sign = (sign == -1 ? -1 : 1);
+		res = res * 10 + (*str++ - '0');
 		i += (res ? 1 : 0);
-		str++;
 		if (i > 19 || res < 0 || (i && !res))
 			return (sign == 1 ? -1 : 0);
 	}
