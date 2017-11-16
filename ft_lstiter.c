1d9d266c2c5664827c3137b287e2f6477a7d9ffb
Artur HRYTSENKO <ahrytsen@e2r13p10.unit.ua>
2017-11-16 20:38:59 +0200

    lstiter NULL protect for func pointer

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/02 19:37:39 by ahrytsen          #+#    #+#             */
-/*   Updated: 2017/11/02 19:40:40 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2017/11/16 20:36:02 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
-	while (lst)
+	while (lst && f)
 	{
 		(*f)(lst);
 		lst = lst->next;
